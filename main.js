0597c19b31e80d42cd1babbc9efd0b69b8206abd
Zach Morse <zacharymorse@gmail.com>
2017-06-16 09:22:46 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+  Here is a guide for the steps you could take:
+*/
+
+// 1. First select and store the elements you'll be working with
+
+// 2. Create your `onSubmit` event for getting the user's search term
+
+// 3. Create your `fetch` request that is called after a submission
+
+axios.get(
+  "https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f"
+);
+
+// 4. Create a way to append the fetch results to your page
+
+// 5. Create a way to listen for a click that will play the song in the audio play
